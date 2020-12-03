1b8a906eac092f0893725671b27af00d7005c5d2
Ivan Chubutkin <ivanchubutkin@rambler.ru>
2020-12-03 19:22:28 +0300

    refactoring

--- app/src/main/java/com/example/searchformovies/MainActivity.java
+++ app/src/main/java/com/example/searchformovies/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.searchformovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView batman=findViewById(R.id.textbatman);
+    TextView devil=findViewById(R.id.textdevil);
+    TextView mutanti=findViewById(R.id.textmutanti);
+    TextView xxx=findViewById(R.id.textxxx);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonbatman(View view) {
+        batman.setTextColor(Color.GRAY);
     }
 }
