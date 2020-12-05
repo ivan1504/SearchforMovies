d21eeff3a3053f87873ce3f1d6f5f51cfa85fcf8
Ivan Chubutkin <ivanchubutkin@rambler.ru>
2020-12-05 21:06:27 +0300

    refactoring

--- app/src/main/java/com/example/searchformovies/MainActivity.java
+++ app/src/main/java/com/example/searchformovies/MainActivity.java
@@ -9,19 +9,37 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView batman=findViewById(R.id.textbatman);
-    TextView devil=findViewById(R.id.textdevil);
-    TextView mutanti=findViewById(R.id.textmutanti);
-    TextView xxx=findViewById(R.id.textxxx);
+    TextView tvbatman;
+    TextView tvdevil;
+    TextView tvmutanti;
+    TextView tvxxx;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvbatman=findViewById(R.id.textbatman);
+        tvdevil=findViewById(R.id.textdevil);
+        tvmutanti=findViewById(R.id.textmutanti);
+        tvxxx=findViewById(R.id.textxxx);
     }
 
     public void buttonbatman(View view) {
-        batman.setTextColor(Color.GRAY);
+
+        tvbatman.setTextColor(Color.GREEN);
+    }
+
+    public void buttondevil(View view) {
+        tvdevil.setTextColor(Color.GREEN);
+    }
+
+    public void buttonmutanti(View view) {
+        tvmutanti.setTextColor(Color.GREEN);
+    }
+
+    public void buttonxxx(View view) {
+        tvxxx.setTextColor(Color.GREEN);
     }
 }
