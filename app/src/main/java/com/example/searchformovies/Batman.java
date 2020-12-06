a0cc4195583f528d9efc4f4af76186b30ca4a5a8
Ivan Chubutkin <ivanchubutkin@rambler.ru>
2020-12-06 20:47:56 +0300

    refactoring

--- app/src/main/java/com/example/searchformovies/Batman.java
+++ app/src/main/java/com/example/searchformovies/Batman.java
@@ -2,7 +2,9 @@ package com.example.searchformovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Batman extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class Batman extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_batman);
+    }
+
+    public void intenbatm(View view) {
+        String textMessage = "Our message";
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
+        sendIntent.setType("text/plain");
+
+        String title = getResources().getString(R.string.chooser_title);
+
+        Intent chooser = Intent.createChooser(sendIntent, title);
+
+
+        if (sendIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
     }
 }
