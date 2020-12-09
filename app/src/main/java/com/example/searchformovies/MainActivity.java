d2f468f36eda432f6eba5b88f0beeb351a07be64
Ivan Chubutkin <ivanchubutkin@rambler.ru>
2020-12-09 15:47:31 +0300

    refactoring

--- app/src/main/java/com/example/searchformovies/MainActivity.java
+++ app/src/main/java/com/example/searchformovies/MainActivity.java
@@ -11,74 +11,48 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    final static String TAG = MainActivity.class.getSimpleName();
-    final static String KEY ="message_key";
     private TextView tvbatman;
     private TextView tvdevil;
     private TextView tvmutanti;
     private TextView tvxxx;
 
 
-   @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        tvbatman = findViewById(R.id.textbatman);
+        tvdevil = findViewById(R.id.textdevill);
+        tvmutanti = findViewById(R.id.textmutanti);
+        tvxxx = findViewById(R.id.textxxx);
 
-       tvbatman = findViewById(R.id.textbatman);
-       tvdevil=findViewById(R.id.textdevill);
-       tvmutanti = findViewById(R.id.textmutanti);
-       tvxxx=findViewById(R.id.textxxx);
-
-
-
-   }
-
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-
-
-
-        tvbatman=findViewById(R.id.textbatman);
-        tvdevil=findViewById(R.id.textdevill);
-        tvmutanti=findViewById(R.id.textmutanti);
-        tvxxx=findViewById(R.id.textxxx);
     }
 
     public void buttonbatman(View view) {
 
         tvbatman.setTextColor(Color.GREEN);
-        Intent batman= new Intent(this, BatmanActivity.class);
+        Intent batman = new Intent(this, BatmanActivity.class);
         startActivity(batman);
     }
 
 
-
     public void buttondevil(View view) {
         tvdevil.setTextColor(Color.GREEN);
-        Intent devil=new Intent(this, DevilActivity.class);
+        Intent devil = new Intent(this, DevilActivity.class);
         startActivity(devil);
 
     }
 
     public void buttonmutanti(View view) {
         tvmutanti.setTextColor(Color.GREEN);
-        Intent mutanti =new Intent(this, MutantiActivity.class);
+        Intent mutanti = new Intent(this, MutantiActivity.class);
         startActivity(mutanti);
     }
 
     public void buttonxxx(View view) {
         tvxxx.setTextColor(Color.GREEN);
-        Intent xxx= new Intent(this, MutantiActivity.class);
+        Intent xxx = new Intent(this, MutantiActivity.class);
         startActivity(xxx);
 
     }
